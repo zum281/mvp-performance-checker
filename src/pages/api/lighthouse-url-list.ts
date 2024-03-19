e517056ebca5004b74d19159366d168c0265cb02
Fulvia <32518183+zum281@users.noreply.github.com>
2024-03-19 15:54:28 +0100

    test: logs

--- src/pages/api/lighthouse-url-list.ts
+++ src/pages/api/lighthouse-url-list.ts
@@ -12,6 +12,8 @@ type ResponseData = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
 	const urlList: string[] = JSON.parse(req.body).urlList;
 
+	console.log(urlList)
+
 	for (const url of urlList) {
 		if (!isValidUrl(url)) res.status(400);
 	}
@@ -19,10 +21,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
 	const options: Flags = { logLevel: "info", output: "json", onlyCategories: ["performance"], port: chrome.port };
 
+	console.log("here 1");
+
 	try {
 		const reports = [];
 		for (const page of urlList) {
 			const runnerResult = await lighthouse(page, options);
+
+			console.log("here 2");
+
 			reports.push(runnerResult!.lhr);
 		}
 
